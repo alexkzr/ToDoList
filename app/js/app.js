1681408e705eb9d53a576e6e4b98fb88b8a7553c
Kozyrev Aleksander <askozyrev@corp.oakmgmt.ru>
2018-01-26 16:38:53 +0300

    1635

--- app/js/app.js
+++ app/js/app.js
@@ -1,94 +1,168 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', e => {
+  e.preventDefault();
+  const submitButton = document.querySelector('.submit');
+  submitButton.type = 'submit';
+  const inputField = document.querySelector('.createItem');
+  const toDoUl = document.querySelector('.toDoUl');
+  const completedUl = document.querySelector('.completedUl');
+  const form = document.querySelector('#header');
+  const tdContainer = document.getElementById('tdContainer');
+  const toDoItems = document.getElementById('toDoItems');
+  (function loadStorage() {
+    if (localStorage.getItem('todo')) {
+      tdContainer.innerHTML = localStorage.getItem('todo');
+    }
+  })();
 
+  function noChildren() {
+    if (toDoUl.hasChildNodes()) {
+      tdContainer.classList.remove('tdContainer');
+    } else {
+      tdContainer.className = 'tdContainer';
+    }
+  }
 
-	const submitButton = document.querySelector('.submit');
-	submitButton.type = 'submit';
-	const inputField = document.querySelector('.createItem');
-	const toDoUl = document.querySelector('.toDoUl');
-	const completedUl = document.querySelector('.completedUl');
-	const form = document.querySelector('#header');
-	const tdContainer = document.getElementById('tdContainer');
-  
-	function noChildren() {
-		if(toDoUl.hasChildNodes()) {
-			tdContainer.classList.remove('tdContainer');
-      
-		} else {
-			tdContainer.className = 'tdContainer';
-      
-		}
-	}
+  var todoLi = '.toDoUl > li';
+  var completedLi = '.completedUl > li';
+  var todoLiElements = document.querySelectorAll(todoLi);
+  var completedLiElements = document.querySelectorAll(completedLi);
+  //Attaching event handlers to the todoUl elements pulled from storage
+  Array.prototype.forEach.call(todoLiElements, todoLi => {
+    var editButton = todoLi.querySelector('.edit');
+    var removeButton = todoLi.querySelector('.remove');
+    var doneButton = todoLi.querySelector('.done');
+    var listItem = todoLi;
+    console.log(editButton);
+    // you can set listeners here
+    editButton.addEventListener('click', e => {
+      e.preventDefault();
+      listItem.contentEditable = 'true';
+    });
 
-	function createLi() {
-		const doneButton = document.createElement('input');
-		const checkedLabel = document.createElement('label');
-		doneButton.type = 'checkbox';
-		checkedLabel.className = 'done';
-		checkedLabel.appendChild(doneButton);
-		const listItem = document.createElement('li');
-		const p = document.createElement('p');
-		const editButton = document.createElement('button');
-		const removeButton = document.createElement('button');
+    listItem.addEventListener('blur', e => {
+      e.preventDefault();
+      listItem.contentEditable = 'false';
+    });
 
-		toDoUl.appendChild(listItem);
-		p.textContent = inputField.value;
-		inputField.value = '';
-		editButton.className = 'edit';
-		removeButton.className = 'remove';
-		listItem.appendChild(checkedLabel);
-		listItem.appendChild(p);
-		listItem.appendChild(editButton);
-		listItem.appendChild(removeButton);
-		doneButton.style.display = 'none';
+    removeButton.addEventListener('click', e => {
+      e.preventDefault();
+      const ul = e.target.parentNode.parentNode;
+      /*const li = e.target.parentNode.parentNode;*/
+      ul.removeChild(e.target.parentNode);
+      noChildren();
+    });
 
-		editButton.addEventListener('click', () => {
-			listItem.contentEditable = 'true';
-		});
+    doneButton.addEventListener('click', e => {
+      e.preventDefault();
+      if (e.target.parentNode.parentNode.className === 'toDoUl') {
+        completedLiElements.push(e.target.parentNode);
+        e.target.parentNode.className = 'removeTransition';
+        noChildren();
+        localStorage.setItem('todo', tdContainer.innerHTML);
+      } else {
+        todoLiElements.push(e.target.parentNode);
+        e.target.parentNode.className = 'addTransition';
+        noChildren();
+      }
+    });
+  });
 
-		listItem.addEventListener('blur', () => {
-			listItem.contentEditable = 'false';
-		});
+  Array.prototype.forEach.call(completedLiElements, completedLi => {
+    var editButton = completedLi.querySelector('.edit');
+    var removeButton = completedLi.querySelector('.remove');
+    var doneButton = completedLi.querySelector('.done');
+    var listItem = completedLi;
+    console.log(editButton);
+    // you can set listeners here
+    editButton.addEventListener('click', e => {
+      e.preventDefault();
+      listItem.contentEditable = 'true';
+    });
 
-		removeButton.addEventListener('click', e => {
-			const ul = e.target.parentNode.parentNode;
-			ul.removeChild(e.target.parentNode);
-			noChildren();
-		});
+    listItem.addEventListener('blur', e => {
+      e.preventDefault();
+      listItem.contentEditable = 'false';
+    });
 
-		doneButton.addEventListener('click', e => {
-			if (e.target.parentNode.parentNode.parentNode
-				.className === 'toDoUl') {
-				completedUl.appendChild(e.target.parentNode.parentNode);
-				e.target.parentNode.parentNode.className = 'removeTransition';
-				noChildren();
+    removeButton.addEventListener('click', e => {
+      e.preventDefault();
+      const ul = e.target.parentNode.parentNode;
+      /*const li = e.target.parentNode.parentNode;*/
+      ul.removeChild(e.target.parentNode);
+      noChildren();
+    });
 
-			} else {
-				toDoUl.appendChild(e.target.parentNode.parentNode);
-				e.target.parentNode.parentNode.className = 'addTransition';
-				noChildren();
-			}
-			localStorage.setItem('todo', tdContainer.innerHTML);
-			
-		});
-		
-	}
+    doneButton.addEventListener('click', e => {
+      e.preventDefault();
+      if (e.target.parentNode.parentNode.parentNode.className === 'toDoUl') {
+        todoLiElements.push(e.target.parentNode.parentNode);
+        e.target.parentNode.parentNode.className = 'removeTransition';
+        noChildren();
+        localStorage.setItem('todo', tdContainer.innerHTML);
+      } else {
+        completedLiElements.push(e.target.parentNode.parentNode);
+        e.target.parentNode.parentNode.className = 'addTransition';
+        noChildren();
+      }
+    });
+  });
 
-	form.addEventListener('submit', e => {
-		e.preventDefault();
-		noChildren();
-		createLi();
-		localStorage.setItem('todo', tdContainer.innerHTML);
+  function createLi() {
+    const wrapper = document.getElementById('wrapper');
+    const doneButton = document.createElement('input');
+    const checkedLabel = document.createElement('label');
+    doneButton.type = 'checkbox';
+    checkedLabel.className = 'done';
+    checkedLabel.appendChild(doneButton);
+    const listItem = document.createElement('li');
+    const p = document.createElement('p');
+    const editButton = document.createElement('button');
+    const removeButton = document.createElement('button');
 
-	});
+    toDoUl.appendChild(listItem);
+    p.textContent = inputField.value;
+    inputField.value = '';
+    editButton.className = 'edit';
+    removeButton.className = 'remove';
+    listItem.appendChild(checkedLabel);
+    listItem.appendChild(p);
+    listItem.appendChild(editButton);
+    listItem.appendChild(removeButton);
+    doneButton.style.display = 'none';
 
-	(function storage() {
+    editButton.addEventListener('click', () => {
+      listItem.contentEditable = 'true';
+    });
 
-		let storage = localStorage.getItem('todo');
-	
-		if (storage !== undefined) {tdContainer.innerHTML = storage;}
-	})();
-	
+    listItem.addEventListener('blur', () => {
+      listItem.contentEditable = 'false';
+    });
 
- 
-  
+    removeButton.addEventListener('click', e => {
+      const ul = e.target.parentNode.parentNode;
+      /*const li = e.target.parentNode.parentNode;*/
+      ul.removeChild(e.target.parentNode);
+      noChildren();
+    });
+
+    doneButton.addEventListener('click', e => {
+      if (e.target.parentNode.parentNode.parentNode.className === 'toDoUl') {
+        completedUl.appendChild(e.target.parentNode.parentNode);
+        e.target.parentNode.parentNode.className = 'removeTransition';
+        noChildren();
+        localStorage.setItem('todo', tdContainer.innerHTML);
+      } else {
+        toDoUl.appendChild(e.target.parentNode.parentNode);
+        e.target.parentNode.parentNode.className = 'addTransition';
+        noChildren();
+      }
+    });
+  }
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    noChildren();
+    createLi();
+    localStorage.setItem('todo', tdContainer.innerHTML);
+  });
 });
